95d3639586ae35bd44cf5119dae20042a241e9a7
rngustmd <hsk1485@gmail.com>
2026-07-08 20:28:44 +0900

    jspractice

---
+++ day03 과제/practice1.js
@@ -0,0 +1,14 @@
+let 이름 = prompt( '이름 :' )
+let 메뉴 = prompt( '메뉴 :' )
+let 수량 = prompt( '수량 :')
+console.log( '[주문접수]' )
+console.log( '이름 :' , 이름 )
+console.log( '메뉴 :' , 메뉴 )
+console.log( '수량 :' , 수량 )
+
+
+
+
+
+
+
