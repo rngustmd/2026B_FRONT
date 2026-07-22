77a81efb664f55c52a227f6dc4c679abc6562697
rngustmd <hsk1485@gmail.com>
2026-07-22 10:56:04 +0900

    day12 1hr

--- day11/practice7.js
+++ day11/practice7.js
@@ -1,43 +1,32 @@
-// 전역배열
-let 전역배열 = [ ]
+
+let 전역배열 = [
+    { 날짜 : '2025-06-19' , 항목 : "점심 식사" , 금액 : 9000 } , 
+    { 날짜 : '2025-06-19' , 항목 : "교통비" , 금액 : 1500 } ]
 
 function 등록함수(){
 
-    let 날짜마크업 = document.querySelector('#date')
-    let 항목마크업 = document.querySelector('#item')
-    let 금액마크업 = document.querySelector('#money')
+    let date = document.querySelector('#date').value; 
+    let item = document.querySelector('#item').value;
+    let money = document.querySelector('#money').value;
 
-    let 날짜 = 날짜마크업.value
-    let 항목 = 항목마크업.value
-    let 금액 = 금액마크업.value
-
-    전역배열.push({
-        date : 날짜,
-        item : 항목,
-        money : 금액
-        }
-    )
-    console.log(전역배열)
-    출력함수()
+    let 객체 = { date , item , money }
+    전역배열.push(객체)
+    alert('등록성공')
 
 } // f end
 
 function 출력함수(){
 
-    // 1. 출력할 html 만들기
     let html = ""
+        for(let index = 0 ; index <= 전역배열.length-1 ; index++){
 
-    // 2. 전역배열 반복
-    for(let index = 0 ; index <= 전역배열.length-1 ; index++){
-
-        html += `<tr>
-                    <td>${전역배열[index].date}</td>
-                    <td>${전역배열[index].item}</td>
-                    <td>${Number(전역배열[index].money).toLocaleString()}</td>
-                 </tr>`
-    }
-
-    // 3. 출력
+            html += `<tr>
+                        <td>${전역배열[index].date}</td>
+                        <td>${전역배열[index].item}</td>
+                        <td>${Number(전역배열[index].money).toLocaleString()}</td>
+                    </tr>`
+        }
+        
     document.querySelector('#tableBody').innerHTML = html
 
 } // f end
